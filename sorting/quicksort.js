815ac22275f00f64147812da7b430328f09fabf1
Ronit Kadam <ronitkadam@Ronits-MBP.attlocal.net>
2021-08-04 16:01:20 -0700

    sorting

--- sorting/quicksort.js
+++ sorting/quicksort.js
@@ -3,38 +3,37 @@
  * @return {number[]}
  */
 var sortArray = function(nums) {
-    if(nums < 2) return nums;
-    
-    quickSortHelper(nums, 0, nums.length);
+    helper(nums, 0, nums.length-1);
     return nums;
 };
 
-function quickSortHelper(nums, startIndex, endIndex) {
-    if(startIndex >= endIndex) return;
+function helper(nums, startIndex, endIndex) {
+    
+    if(startIndex > endIndex) return;
     let pivotIndex = startIndex + Math.floor((endIndex - startIndex)/2);
-    let pivotValue = nums[pivotIndex];
+    // Lomoto's Partitioning: 
     swap(nums, startIndex, pivotIndex);
-    let green = startIndex + 1;
     let orange = startIndex + 1;
-    while(green <= endIndex) {
-        if(nums[green] <= pivotValue) {
-            swap(nums, orange, green);
-            green = green + 1;
+    let green = startIndex + 1;
+    while(orange <= endIndex) {
+        if(nums[orange] > nums[startIndex]) {
             orange = orange + 1;
         } else {
+            swap(nums, green, orange);
             green = green + 1;
+            orange = orange + 1;
         }
     }
-    swap(nums, startIndex, orange-1);
-    // Ensure the pivotIndex is updated after the partion
-    pivotIndex = orange - 1;
-    quickSortHelper(nums, startIndex, pivotIndex-1);
-    quickSortHelper(nums, pivotIndex+1, endIndex);
+    green = green - 1;
+    pivotIndex = green;
+    swap(nums, startIndex, pivotIndex);
+
+    helper(nums, startIndex, pivotIndex-1);
+    helper(nums, pivotIndex+1, endIndex);
 }
 
-
-function swap(array, index1, index2) {
-    let temp = array[index1];
-    array[index1] = array[index2];
-    array[index2] = temp;
+function swap(nums, leftIndex, rightIndex) {
+    let temp = nums[leftIndex];
+    nums[leftIndex] = nums[rightIndex];
+    nums[rightIndex] = temp;
 }
